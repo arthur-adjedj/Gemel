4a302eb3c7e10fd6d770f897af9f94121dce47e8
arthur-adjedj <arthur.adjedj@gmail.com>
2026-04-07 14:30:07 +0200

    feat: finish elaboration core logic for match extensions

--- LeanALaCarte/ExtendMatch.lean
+++ LeanALaCarte/ExtendMatch.lean
@@ -2,13 +2,13 @@ module
 
 public import Lean
 public import LeanALaCarte.ModMap
-
-@[expose] public section
+import all Lean.Elab.Match
+@[expose] section
 
 open Lean Elab Term Meta Match
 
 structure MatcherBundle where
-  discrs : Array (Expr × DiscrInfo)
+  discrs : Array Discr
   matchType : Expr
   lhss : List AltLHS
   rhss : Array Expr
@@ -27,7 +27,7 @@ def mkMatcherBundle (e : Expr) : MetaM (Option MatcherBundle) :=
   let matchType ← instantiateForall (← instantiateTypeLevelParams (← getConstVal c) us) params
   -- let motive := args[info.numParams...1]
   let discrsExpr := args[info.getFirstDiscrPos...(info.getFirstDiscrPos + info.numDiscrs)]
-  let discrs := discrsExpr.toArray.zip info.discrInfos
+  let discrs := discrsExpr.toArray.zipWith (bs := info.discrInfos) fun e h => ⟨e,h.1.map (TSyntax.raw ∘ mkIdent)⟩
   let rhss := args[info.getFirstAltPos...(info.getFirstAltPos + info.numAlts)]
   let altTys : Array Expr ← forallTelescopeReducing matchType fun matchBinders _ => matchBinders[info.getFirstAltPos...(info.getFirstAltPos + info.numAlts)].toArray.mapM inferType
   let lhss : Array AltLHS ← altTys.mapM fun altTy =>  forallTelescopeReducing altTy fun _ e => e.withApp fun _motive margs => do
@@ -52,31 +52,32 @@ partial def Lean.Meta.Match.Pattern.modMap (map : ModularMap) : Pattern → Meta
 
 def MatcherBundle.modMap (map : ModularMap) (m : MatcherBundle) : MetaM MatcherBundle := do
   let {discrs, matchType, lhss, rhss} := m
-  let discrs ← discrs.mapM fun (expr, h?) => do return (← _root_.modMap map expr, h?)
+  let discrs ← discrs.mapM fun ⟨expr, h?⟩ => do return ⟨← _root_.modMap map expr, h?⟩
   let matchType ← _root_.modMap map matchType
   let rhss ← rhss.mapM (_root_.modMap map)
   return {discrs, matchType, lhss, rhss}
 
 def MatcherBundle.mkMatcher (m : MatcherBundle) (addedAlts : Array TermMatchAltView): TermElabM Expr := do
   let {discrs, matchType, lhss, rhss} := m
-  let (discrs, matchType, altLHSS, rhss) ← commitIfDidNotPostpone do
+  let (discrs, matchType, newlhss, newrhss) ← commitIfDidNotPostpone do
     let matchAlts ← liftMacroM <| expandMacrosInPatterns addedAlts
     trace[Elab.match] "matchType: {matchType}"
-    let alts : Array (_ × _) := sorry --TODO remove once `elabMatchAltViews` is actually usable`
-    -- let (discrs, matchType, alts) ← elabMatchAltViews false discrs matchType matchAlts --TODO allow generalisations ?
+    let (discrs, matchType, alts, _) ← elabMatchAltViews false discrs matchType matchAlts --TODO allow generalisations ?
     synthesizeSyntheticMVarsUsingDefault
     let rhss := alts.map Prod.snd
     let matchType ← instantiateMVars matchType
     let altLHSS ← instantiateAltLHSs (alts.map Prod.fst)
     return (discrs, matchType, altLHSS, rhss)
+  let lhss := lhss ++ newlhss
+  let rhss := rhss ++ newrhss
   let numDiscrs := discrs.size
   let matcherName ← mkAuxName `match
-  let matcherResult ← Lean.Elab.Term.mkMatcher { matcherName, matchType, lhss, discrInfos := discrs.map Prod.snd}
+  let matcherResult ← Lean.Elab.Term.mkMatcher { matcherName, matchType, lhss, discrInfos := discrs.map fun ⟨_,h⟩ => ⟨h.map Syntax.getId⟩}
   reportMatcherResultErrors lhss matcherResult
   matcherResult.addMatcher
   let motive ← forallBoundedTelescope matchType numDiscrs fun xs matchType => mkLambdaFVars xs matchType
   let r := mkApp matcherResult.matcher motive
-  let r := mkAppN r (discrs.map Prod.fst)
+  let r := mkAppN r (discrs.map Discr.expr)
   let r := mkAppN r rhss
   trace[Elab.match] "result: {r}"
   return r
