209374032c9aa997aabda671f4747e8de0412bc6
arthur-adjedj <arthur.adjedj@gmail.com>
2026-04-13 10:04:45 +0200

    feat : some progress on STLC formalisation

--- LeanALaCarte/Test/STLC.lean
+++ LeanALaCarte/Test/STLC.lean
@@ -19,6 +19,8 @@ inductive Var  : List Ty → Ty → Type
 
 def Ren {T} (Γ Δ : List T) := ∀ A, Γ.Has A → Δ.Has A
 
+def Ren.id : Ren Γ Γ := fun _ h => h
+
 def Ren.ext (R : Ren Γ Δ) A: Ren (A::Γ) (A::Δ)
   | _,.head => .head
   | _,.tail h => .tail (R _ h)
@@ -28,10 +30,16 @@ def Ren.wk : Ren Γ (A::Γ) := fun _ h => .tail h
 def Var.wk (R : Ren Γ Δ) : Var Γ B → Var Δ B
   | var h => .var (R _ h)
 
-def Var.Subst (Γ Δ : List Ty) := ∀ {A}, Γ.Has A → Var Δ A
+def Var.Subst (Γ Δ : List Ty) := ∀ ⦃A⦄, Γ.Has A → Var Δ A
+
+def Var.Subst.id : Var.Subst Γ Γ := fun _ h => .var h
 
 def Var.subst (s : Var.Subst Γ Δ) : Var Γ A → Var Δ A
   | .var h => s h
+
+theorem Var.subst_id : Var.subst Var.Subst.id t = t := by
+  induction t
+  rfl
 
 inductive Var.Red : Var Γ A → Var Γ A → Prop where
 
@@ -56,6 +64,14 @@ modular
       | .const a => .const a
       | .plus a b => .plus (NatExt.subst s a) (NatExt.subst s b)
 
+  mod_def NatExt.Subst.id extends Var.Subst.id
+
+  mod_def NatExt.subst_id extends Var.subst_id where
+    finally
+      · intros; rfl
+      · intro Γ a b a_ih b_ih
+        rw [NatExt.subst,a_ih,b_ih]
+
   inductive NatExt.Red extends Var.Red where
     | plus_const : NatExt.Red (.plus (.const a) (.const b)) (.const (a + b))
     | plus_lhs : NatExt.Red a₁ a₂ → NatExt.Red (.plus a₁ b) (.plus a₂ b)
@@ -74,11 +90,21 @@ modular
       | _, .lam f => .lam (Term.wk (Ren.ext R _) f)
       | _, .app a b => .app (Term.wk R a) (Term.wk R b)
 
-  mod_def Term.Subst extends NatExt.Subst
+  -- theorem Term.wk_wk : Term.wk
 
-  def Term.subst_ext (s : Term.Subst Γ Δ): ∀ {B}, (A::Γ).Has B → Term (A::Δ) B
+  mod_def Term.Subst extends NatExt.Subst
+  mod_def Term.Subst.id extends NatExt.Subst.id
+
+  def Term.subst_ext (s : Term.Subst Γ Δ): Term.Subst (A::Γ) (A::Δ)
     | _,.head => .var .head
     | _,.tail h => Term.wk Ren.wk (s h)
+
+  theorem Term.subst_ext_id : @Term.subst_ext Γ Γ A Term.Subst.id = Term.Subst.id := by
+    funext _ h
+    cases h
+    · rfl
+    · rw [subst_ext]
+      sorry
 
   def Term.subst_wk (a : Term Γ A) : Term.Subst (A::Γ) Γ
     | _,.head => a
@@ -88,6 +114,13 @@ modular
     match_1 with
       | _, .lam f => .lam (Term.subst (Term.subst_ext s) f)
       | _, .app a b => .app (Term.subst s a) (Term.subst s b)
+
+  mod_def Term.subst_id extends NatExt.subst_id where
+    finally
+      · intro _ _ _ a a_ih
+        rw [Term.subst, Term.subst_ext_id,a_ih]
+      · intro _ _ _ a b a_ih b_ih
+        rw [Term.subst,a_ih,b_ih]
 
   inductive Term.Red extends NatExt.Red where
     | beta : Term.Red (.app (.lam f) x) (Term.subst (Term.subst_wk x) f)
