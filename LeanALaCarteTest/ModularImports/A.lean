a1df07afe4e8918db165e05ea032629b282cac93
arthur-adjedj <arthur.adjedj@gmail.com>
2026-07-08 15:52:08 +0200

    fix test outputs

--- LeanALaCarteTest/ModularImports/A.lean
+++ LeanALaCarteTest/ModularImports/A.lean
@@ -1,7 +1,12 @@
 import LeanALaCarte
 
 set_option trace.Modular.Elab true
-/-- trace: [Modular.Elab] [] -/
+/--
+trace: [Modular.Elab] []
+---
+trace: [Modular.Elab] ✅️ run_cmd
+        return
+-/
 #guard_msgs in
 modular (name := `A1)
   -- empty command
@@ -9,7 +14,12 @@ modular (name := `A1)
 
 namespace A
 
-/-- trace: [Modular.Elab] [_modular.A1] -/
+/--
+trace: [Modular.Elab] [_modular.A1]
+---
+trace: [Modular.Elab] ✅️ run_cmd
+        return
+-/
 #guard_msgs in
 modular (name := `A2) (imports := #[`A1])
   -- empty command
